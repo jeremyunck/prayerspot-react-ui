c91d939187b602c20895fc197541a7f5afc62f67
Jeremy Unck <jeremyunck@gmail.com>
2023-04-20 13:40:42 -0500

    create user post form working

--- src/components/UserPostForm.jsx
+++ src/components/UserPostForm.jsx
@@ -1,10 +1,40 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import Axios from "axios";
 
 function UserPostForm() {
   const { register, handleSubmit } = useForm();
   const onSubmit = (data) => {
     console.log(data);
+    const postData = { caption: data.caption };
+    console.log(postData);
+
+    Axios.post("http://localhost:8080/api/v1/user-post/create", postData)
+      .then((res) => {
+        console.log("Successfully created post");
+        const file = data.image[0];
+        const formData = new FormData();
+        formData.append("file", file);
+
+        Axios.post(
+          `http://localhost:8080/api/v1/user-post/${res.data}/image/upload`,
+          formData,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        )
+          .then(() => {
+            console.log("Image uploaded successfully");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
